7d23aeb84c8d595c369c8553aaeacd5e68518872
asomeJay <museum114@naver.com>
2020-02-09 12:52:39 +0900

    #2.11 MVC Pattern part Three

--- Router/globalRouter.js
+++ Router/globalRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import routes from "../routes";
+import { home , search} from "../controllers/videoController";
+import { join, logout, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send('Home'));
-globalRouter.get(routes.join, (req, res) => res.send('Join'));
-globalRouter.get(routes.login, (req, res) => res.send('logint'));
-globalRouter.get(routes.logout, (req, res) => res.send('Hlong out'));
-globalRouter.get(routes.search, (req, res) => res.send('search'));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search);
 
 export default globalRouter;
