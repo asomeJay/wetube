984e538f3a2f68df1dec29b228687b56a197cf52
asomeJay <museum114@naver.com>
2020-02-17 00:36:25 +0900

    go

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ import helmet from "helmet";
 import cookie_parser from "cookie-parser";
 import body_parser from "body-parser";
 import userRouter from "./Router/userRouter";
+import { localsMiddleware } from "./middlewares";
 import videoRouter from "./Router/videoRouter";
 import globalRouter from "./Router/globalRouter";
 import routes from "./routes";
+
 
 const app = express()
 //Node Module을 다른 곳에서 들고온다. require가 하는 일은
 // Node Module을 들고오는 것. express라는 이름의 폴더를
 // 내 파일들 속에서 찾으려함 
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookie_parser());
 app.use(body_parser.urlencoded({extended : true}));
 app.use(body_parser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,6 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+export const search = (req, res) => res.render("search", {pageTitle: "Search"});
+export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "VideoDetail"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "EditVideo"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "DeleteVideo"});
