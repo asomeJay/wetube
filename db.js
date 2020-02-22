5aab03c30e6e13bc2e8028988354de204a5ff7cb
asomeJay <museum114@naver.com>
2020-02-22 18:33:09 +0900

    #2.21 Home Controller
    pug의 iteration 문법을 배웠다.
    가짜 db를 만들어서 비디오의 상세 내용을 표현하는 법도 익힘

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324393,
+        title : 'Fucking',
+        description : 'This is smth i lvoe',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            name:"nicola",
+            email:"nico@aa.net",
+            id : 1212 
+        }
+    },
+    {
+        id:311111113,
+        title : 'sddg',
+        description : 'Thsdsdsda i lvoe',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            name:"nicola",
+            email:"nico@aa.net",
+            id : 1212 
+        }
+    },
+    {
+        id:333,
+        title : 'F131313ng',
+        description : 'This is smth i lvoe',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            name:"nicola",
+            email:"nico@aa.net",
+            id : 1212 
+        }
+    },
+    {
+        id:32437771,
+        title : 'Fasdasdg',
+        description : 'Thaaaaaaaaaaaaaaaaaaaaaaaaaaaaalvoe',
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            name:"nicola",
+            email:"nico@aa.net",
+            id : 1212 
+        }
+    }
+];
