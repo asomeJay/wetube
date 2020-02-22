e6d282c0a82b20e3f2d61eb6bb0770a374618617
asomeJay <museum114@naver.com>
2020-02-22 17:59:35 +0900

    #2.18

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # WeTube
+
 Cloning Youtube with Vanilla and NodeJS
 
+## Pages:
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => res.render("search", {pageTitle: "Search"});
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
-export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "VideoDetail"});
-export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "EditVideo"});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "DeleteVideo"});
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>{
+    const {query: { term : searchingBy }} = req;
+    res.render("search", {pageTitle : "Search", searchingBy});
+}
+
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
