f672a34daf5df77e0965c2e0d5e29fdb4674a0d8
asomeJay <museum114@naver.com>
2020-02-29 20:10:35 +0900

    #3.4 Comment Model
    Comment Model도 Video Model과 같은 형식으로 작동.
    Comment와 Video 사이 Realtion을 성립시키는 방법은 두 가지
    Comment에서 Video의 ID를 기억하거나
    Video에서 Comment의 Array를 가지거나
    이번 수업에는 Video가 Comment의 Array를 가지도록 한다.

--- models/Video.js
+++ models/Video.js
@@ -1,23 +1,27 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-fileUrl: {
-    type: String,
-    required: "File Url is required"
-},
-title:{
-    type: String,
-    required: "Title is required"
-},
-description: String,
-view:{
-    type:Number,
-    default:0
-},
-createdAt:{
-    type: Date,
-    default: Date.now
-}    
+    fileUrl: {
+        type: String,
+        required: "File Url is required"
+    },
+    title: {
+        type: String,
+        required: "Title is required"
+    },
+    description: String,
+    view: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
 });
 
 const model = mongoose.model("Video", VideoSchema);
