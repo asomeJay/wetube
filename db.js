f7add7a50dd433c44f56d9a04d0d8e5d733e3993
asomeJay <museum114@naver.com>
2020-02-29 18:55:39 +0900

    #3.1 Connecting to MongoDB
    MongoDB connection을 배웠고, 앞으로 dotenv에 대해서 배우게 될 것

--- db.js
+++ db.js
@@ -1,50 +1,14 @@
-export const videos = [
-    {
-        id:324393,
-        title : 'Fucking',
-        description : 'This is smth i lvoe',
-        views : 24,
-        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:"nicola",
-            email:"nico@aa.net",
-            id : 1212 
-        }
-    },
-    {
-        id:311111113,
-        title : 'sddg',
-        description : 'Thsdsdsda i lvoe',
-        views : 24,
-        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:"nicola",
-            email:"nico@aa.net",
-            id : 1212 
-        }
-    },
-    {
-        id:333,
-        title : 'F131313ng',
-        description : 'This is smth i lvoe',
-        views : 24,
-        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:"nicola",
-            email:"nico@aa.net",
-            id : 1212 
-        }
-    },
-    {
-        id:32437771,
-        title : 'Fasdasdg',
-        description : 'Thaaaaaaaaaaaaaaaaaaaaaaaaaaaaalvoe',
-        views : 24,
-        videoFile : "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            name:"nicola",
-            email:"nico@aa.net",
-            id : 1212 
-        }
-    }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () =>console.log("HI CONNECTED to DB");
+const handleError = error => console.log(`Error on DB connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
