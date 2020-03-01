39a22de7f1c8502000c623abde26b4f9f27a6655
asomeJay <museum114@naver.com>
2020-03-02 00:12:18 +0900

    #3.9 Editing a Video
    1. Edit Video 버튼을 만든다.
    2. 누르면 해당 Video 번호를 주소로 가지는 route 이동!
    3. title과 description을 수정할 수 있는 textarea가 나타난다. 그걸 수정하면 된다.
    Video의 Model을 req로 받을 수 있는 것과 동일하게 만듬으로써 코드를 더욱 간결하게 짤 수 있다.

--- Router/videoRouter.js
+++ Router/videoRouter.js
@@ -4,8 +4,9 @@ import {
     getUpload,
     postUpload,
     videoDetail,
-    editVideo,
-    deleteVideo
+    deleteVideo,
+    getEditVideo,
+    postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
@@ -13,8 +14,12 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter
